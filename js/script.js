6020bb29199bce6754db174db9bf60795c81a8ef
Vogelk-Audiovisual <vogelk.audiovisual@gmail.com>
2026-01-31 12:48:55 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 const portfolioData = [
     // Fotografia (6 imagens)
     { id: 1, category: 'fotografia', src: 'images/nic-02.jpeg', title: 'Paisagem Cinematográfica' },
-    { id: 2, category: 'fotografia', src: 'images/IMG_7993.jpg', title: 'Arquitetura Urbana' },
+    { id: 2, category: 'fotografia', src: 'images/onibus_curitiba.jpeg', title: 'Arquitetura Urbana' },
     { id: 3, category: 'fotografia', src: 'images/img_1682.jpg', title: 'Retrato Profissional' },
     { id: 4, category: 'fotografia', src: 'images/paisagem-05.JPEG', title: 'Detalhe da Natureza' },
     { id: 5, category: 'fotografia', src: 'images/Tibagi-Guartelá-276.jpg', title: 'Fotografia de Rua' },
